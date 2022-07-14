4e220a89e49c007c42e97ac998ee7acd39458eea
ken0814 <1106103247@nkust.edu.tw>
2022-07-14 16:32:33 +0800

    update
    ಥ_ಥ

---
+++ js/button.js
@@ -0,0 +1,13 @@
+const btns = document.querySelectorAll('.btn-white')
+for (let btn of btns) {
+  btn.onclick = () => {
+    location.href = '#'
+  }
+}
+
+const btns2 = document.querySelectorAll('.btn-primary')
+for (let btn of btns2) {
+  btn.onclick = () => {
+    location.href = '#'
+  }
+}
